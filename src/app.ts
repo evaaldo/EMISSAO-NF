40f9cf1cb231fd2be7d829da1b733f92f79f497a
evaldo <evaaldormj@gmail.com>
2024-02-20 20:01:16 -0300

    fix: add router to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { router } from './routes'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(router)
 
 app.listen(port, () => console.log(`Application is running on: http://localhost:${port}`))
