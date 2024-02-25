7e92b837249d4d040b0ff1dcc48ce848bdc7ba0e
evaldo <evaaldormj@gmail.com>
2024-02-24 23:39:55 -0300

    feat: add createCourse controller

--- src/controllers/CourseController.ts
+++ src/controllers/CourseController.ts
@@ -7,6 +7,16 @@ export class CourseController {
 
     createCourse(request: Request, response: Response) {
 
+        const { title, educator, description } = request.body
+
+        courseService.createCourse(title, educator, description)
+
+        try {
+            return response.status(200).json({ message: 'Course created!' })
+        } catch(error) {
+            return response.status(400).json(error)      
+        }
+
     }
 
 }
