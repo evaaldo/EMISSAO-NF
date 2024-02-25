1edcc51544a748f33690a06fdaca59fb48aa2c6d
evaldo <evaaldormj@gmail.com>
2024-02-24 23:41:08 -0300

    feat: add createCourse route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from 'express'
+import { CourseController } from './controllers/CourseController'
 
 export const router = Router()
+const courseController = new CourseController()
 
 // Public route
 
 router.get('/', (request: Request, response: Response) => {
     return response.status(200).json({ message: 'This is the public route' })
 })
+
+// Course Routes
+
+router.post('course', courseController.createCourse)
