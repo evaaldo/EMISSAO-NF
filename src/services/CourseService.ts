f512f8160d10be67858d4eaf149bc35e9d89cc4e
evaldo <evaaldormj@gmail.com>
2024-02-24 23:38:00 -0300

    feat: add createCourse service

--- src/services/CourseService.ts
+++ src/services/CourseService.ts
@@ -1,7 +1,13 @@
+import { sql } from '../database/connection'
+
 export class CourseService {
 
-    createCourse() {
+    async createCourse(title: string, educator: string, description: string) {
+
+        const courseId: any = crypto.randomUUID()
         
+        await sql`INSERT INTO courses (id, title, educator, description) VALUES (${courseId},${title},${educator},${description})`
+
     }
 
 }
