f3526ad1d9b11be9cdb332480c275c36111559ba
evaldo <evaaldormj@gmail.com>
2024-03-03 12:13:03 -0300

    feat: add verifyIfCourseAlreadyExists middleware

--- src/middlewares/verifyIfExists.ts
+++ src/middlewares/verifyIfExists.ts
@@ -25,4 +25,26 @@ export class verifyExistence {
 
     }
 
+    async verifyIfCourseAlreadyExists(request: Request, response: Response, next: NextFunction) {
+
+        const { title } = request.body
+
+        try {
+
+            const course = await sql`SELECT * FROM courses WHERE title = ${title}`
+
+            if(course && course.length > 0) {
+                return response.status(400).json({ error: "The course already exists!" })
+            } else {
+                next()
+            }
+
+        } catch(error) {
+
+            return response.status(500).json({ error: `Internal server error: ${error}` })
+
+        }
+
+    }
+
 }
