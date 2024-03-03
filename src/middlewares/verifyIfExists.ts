41390a141c16e41c22718392fda4b8ce4136905f
evaldo <evaaldormj@gmail.com>
2024-03-03 12:10:02 -0300

    feat: add verifyIfCourseExists middleware

---
+++ src/middlewares/verifyIfExists.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { sql } from "../database/connection";
+
+export class verifyExistence {
+
+    async verifyIfCourseExists(request: Request, response: Response, next: NextFunction) {
+
+        const { title } = request.body
+
+        try {
+
+            const course = await sql`SELECT * FROM courses WHERE title = ${title}`
+
+            if(course && course.length > 0) {
+                next()
+            } else {
+                return response.status(400).json({ error: "The course doesn't exists!" })
+            }
+
+        } catch(error) {
+
+            return response.status(500).json({ error: `Internal server error: ${error}` })
+
+        }
+
+    }
+
+}
