429553b36714c50d40df89dcdeeba5fcaea0e81c
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2024-10-11 11:16:08 -0300

    Delete dist directory

--- dist/services/CourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CourseService = void 0;
-class CourseService {
-    createCourse() {
-    }
-}
-exports.CourseService = CourseService;
